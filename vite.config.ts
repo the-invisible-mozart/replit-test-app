84f1dbf766d4b2090b9305c619351ed15e7308fd
Mozart Diniz <mozart.diniz@invisible.email>
2025-09-04 14:08:52 +0100

    changign port back to 5000

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5001,
+    port: 5000,
     strictPort: true,
     allowedHosts: true,
   },
   preview: {
-    port: 5001,
+    port: 5000,
   },
-})
+});
