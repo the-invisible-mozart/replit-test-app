d3ccb4e07f61f440e1b55425ca0958309a7bf204
Mozart Diniz <mozart.diniz@invisible.email>
2025-09-04 14:03:57 +0100

    updating dependencies version and some configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5001,
+    strictPort: true,
+    allowedHosts: true,
+  },
+  preview: {
+    port: 5001,
+  },
 })
-
-
